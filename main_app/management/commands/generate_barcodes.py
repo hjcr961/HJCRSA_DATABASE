d05a70e693b820f46a34fc94d5635a79603587bf
hjcr961 <hjcrsadatabase@gmail.com>
2025-09-13 15:27:32 +0200

    Add files via upload

--- main_app/management/commands/generate_barcodes.py
+++ main_app/management/commands/generate_barcodes.py
@@ -1,11 +1,24 @@
 from django.core.management.base import BaseCommand
-from main_app.barcode_utils import generate_barcode_for_evangelists
+from main_app.barcode_utils import generate_barcode_for_evangelists, recreate_all_barcodes
 
 class Command(BaseCommand):
-    help = 'Generate barcodes for members with EVANGELIST title'
+    help = 'Generate barcodes for members with eligible titles'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--recreate',
+            action='store_true',
+            help='Recreate all existing barcodes for eligible members',
+        )
 
     def handle(self, *args, **options):
-        count = generate_barcode_for_evangelists()
-        self.stdout.write(
-            self.style.SUCCESS(f'Successfully created {count} barcode entries for evangelists')
-        )
+        if options['recreate']:
+            count = recreate_all_barcodes()
+            self.stdout.write(
+                self.style.SUCCESS(f'Successfully recreated {count} barcode entries for all eligible members')
+            )
+        else:
+            count = generate_barcode_for_evangelists()
+            self.stdout.write(
+                self.style.SUCCESS(f'Successfully created {count} new barcode entries for eligible members')
+            )
