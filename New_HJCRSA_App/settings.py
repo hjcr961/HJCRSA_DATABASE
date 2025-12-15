d563ae8c73d2e601b6aaea4f2f72515148703bb7
hjcr961 <hjcrsadatabase@gmail.com>
2025-12-15 10:56:10 +0200

    Update settings.py removed secret key

--- New_HJCRSA_App/settings.py
+++ New_HJCRSA_App/settings.py
@@ -28,8 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!p+3%_e0osy+bo(%0f)g7lkcr&e+4^jq4epy(z$h+@&%%4-d$$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Adjust DEBUG based on environment
